6bb3b71645597dbf959f3f9706ebf7d4ccc7c8d5
tgi74000 <tgi74@hotmail.fr>
2016-11-23 17:35:43 +0100

    initial commit

---
+++ ObsGitBranchDisplayer/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace ObsGitBranchDisplayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textboxFrom = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.readTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxFormat = new System.Windows.Forms.TextBox();
+            this.textBoxTxt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textboxFrom
+            // 
+            this.textboxFrom.Location = new System.Drawing.Point(82, 6);
+            this.textboxFrom.Name = "textboxFrom";
+            this.textboxFrom.Size = new System.Drawing.Size(346, 20);
+            this.textboxFrom.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // readTimer
+            // 
+            this.readTimer.Enabled = true;
+            this.readTimer.Interval = 2000;
+            this.readTimer.Tick += new System.EventHandler(this.readTimer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "HEAD path:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Format: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = ".txt Save Location: ";
+            // 
+            // textBoxFormat
+            // 
+            this.textBoxFormat.Location = new System.Drawing.Point(63, 47);
+            this.textBoxFormat.Name = "textBoxFormat";
+            this.textBoxFormat.Size = new System.Drawing.Size(365, 20);
+            this.textBoxFormat.TabIndex = 4;
+            this.textBoxFormat.Text = "Branch: {0}";
+            // 
+            // textBoxTxt
+            // 
+            this.textBoxTxt.Location = new System.Drawing.Point(117, 95);
+            this.textBoxTxt.Name = "textBoxTxt";
+            this.textBoxTxt.Size = new System.Drawing.Size(311, 20);
+            this.textBoxTxt.TabIndex = 5;
+            this.textBoxTxt.Text = "branch.txt";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 250);
+            this.Controls.Add(this.textBoxTxt);
+            this.Controls.Add(this.textBoxFormat);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textboxFrom);
+            this.Name = "Form1";
+            this.Text = "ObsGitBranchDisplayer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textboxFrom;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Timer readTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxFormat;
+        private System.Windows.Forms.TextBox textBoxTxt;
+    }
+}
+
---
+++ ObsGitBranchDisplayer/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ObsGitBranchDisplayer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string GetBranchName(string url)
+        {
+            return File.ReadAllText(url).Split('/')[2];
+        }
+
+        private void onFromClick(object sender, EventArgs args)
+        {
+            DialogResult r = openFileDialog1.ShowDialog();
+            if(r == DialogResult.OK)
+            {
+                textboxFrom.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void onTxtClick(object sender, EventArgs args)
+        {
+            DialogResult r = openFileDialog1.ShowDialog();
+            if (r == DialogResult.OK)
+            {
+                textBoxTxt.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void onFormatClick(object sender, EventArgs args)
+        {
+            DialogResult r = openFileDialog1.ShowDialog();
+            if (r == DialogResult.OK)
+            {
+                textBoxFormat.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void readTimer_Tick(object sender, EventArgs e)
+        {
+            if(textboxFrom.Text != null && textboxFrom.Text != "")
+                File.WriteAllText("branch.txt", string.Format(textBoxFormat.Text, GetBranchName(textboxFrom.Text)));
+        }
+    }
+}
